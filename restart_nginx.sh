870583fc44aece14e5552c3831002fdab7546e70
Marcin Marczyk <m.l.marczyk@gmail.com>
2018-07-11 21:28:44 +0200

    Update restart_nginx.sh

--- restart_nginx.sh
+++ restart_nginx.sh
@@ -1,2 +1,2 @@
 docker rm -f $(docker ps -a | grep nginx | awk '{print $1}' | grep -v CONTAINER)
-docker run -d -p 80:80 -v /home/pi/nginx.conf:/etc/nginx/nginx.conf:ro -v /home/pi/.htpasswd:/etc/nginx/.htpasswd:ro --restart=unless-stopped --name nginx --network custom-net marczykm/custom-nginx
+docker run -d -p 80:80 -v /home/pi/nginx.conf:/etc/nginx/nginx.conf:ro -v /home/pi/.htpasswd:/etc/nginx/.htpasswd:ro --restart=unless-stopped --name nginx --network custom-net marczykm/custom-nginx:rpi
